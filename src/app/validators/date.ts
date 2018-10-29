811fb290d1d6ee62cb5a6f72897b22b66c8b46b7
Peko Roganović <peko.roganovic@gmail.com>
2018-10-29 15:07:02 +0100

    year validation

--- src/app/validators/date.ts
+++ src/app/validators/date.ts
@@ -4,9 +4,14 @@ import * as moment from 'moment';
 export class DateValidator {
 
     static isValid(control: FormControl): any {
-        if (!moment(control.value, 'YYYYMMDD').isValid()) {
-        return {'bad format': true};
-    }
+        const d = moment(control.value, 'YYYYMMDD');
+        if (!d.isValid()) {
+            return {'invalid format': true};
+        } else {
+            if (d.year() < 2018 || d.year() > 2040) {
+                return {'invalid year': true};
+            }
+        }
     return null;
 }
 }
