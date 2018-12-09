1b07096591fde657d2b7292a4a67fa3b75155f69
RPeko <peko.roganovic@gmail.com>
2018-12-09 16:41:28 +0100

    prepare heroku

--- server.js
+++ server.js
@@ -1,31 +1,14 @@
-// const path = require('path');
-// const express = require('express');
-// const app = express();
-
-// // Serve static files
-// app.use(express.static(__dirname + '/dist/showdog-b'));
-
-// // Send all requests to index.html
-// app.get('/*', function(req, res) {
-//   res.sendFile(path.join(__dirname + '/dist/showdog-b/index.html'));
-// });
-
-// // default Heroku port
-// app.listen(process.env.PORT || 5000);
-
-//Install express server
-const express = require('express');
 const path = require('path');
-
+const express = require('express');
 const app = express();
 
-// Serve only the static files form the dist directory
-// Replace the '/dist/<to_your_project_name>'
+// Serve static files
 app.use(express.static(__dirname + '/dist/showdog-b'));
 
-app.get('*', function(req,res) {
-  // Replace the '/dist/<to_your_project_name>/index.html'
-  res.sendFile(path.join(__dirname+ '/dist/showdog-b/index.html'));
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/showdog-b/index.html'));
 });
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+
+// default Heroku port
+app.listen(process.env.PORT || 5000);
