eff996e5eeb8fb2cf9a0c2d06689a194067801c9
RPeko <peko.roganovic@gmail.com>
2018-12-09 13:42:37 +0100

    heroku prepare

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: true,
+  production: false,
   firebaseConfig: {
     apiKey: 'AIzaSyCf6lecb7d201g5c4b27oVO5Uer5MI7l8c',
     authDomain: 'showdogpro.firebaseapp.com',
@@ -12,3 +12,12 @@ export const environment = {
     apiKey: 'AIzaSyDDtxkjnMS-rXozS7c4q43JcmB0xNffplg'
   }
 };
+
+
+/*
+ * In development mode, to ignore zone related error stack frames such as
+ * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
+ * import the following file, but please comment it out in production mode
+ * because it will have performance impact when throw error
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
